56c7e3d4b17de77cd8dc30db2dd234a64772b12f
spsingh559 <shyam.singh4@wipro.com>
2018-04-19 21:25:55 +0530

    Boilerplate

---
+++ app/webclient/components/Home.jsx
@@ -0,0 +1,73 @@
+
+import React from 'react';
+import { Link } from 'react-router';
+import Axios from 'axios';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+// import {Grid} from 'react-bootstrap';
+import {Grid,Row,Col,Carousel} from 'react-bootstrap';
+
+
+// Component End
+
+// Allocation data
+
+
+// const style={
+//   instanceCountStyle:{
+//     height:"100px",
+//     width:"320px",
+//     borderRadius: "6px",
+//     border: "solid 1px #d5d5d5",
+//     align:"center"
+//   },
+//   instanceProjectStyle:{
+//     height:"100px",
+//     width:"320px",
+//     borderRadius: "6px",
+//     border: "solid 1px #d5d5d5",
+//     align:"center",
+//     marginLeft:"70px"
+//   },
+//   CloudChartStyle:{
+//     height:"400px",
+//     marginTop:"50px",
+//     width:"auto", 
+//     borderRadius: "6px",
+//     border: "solid 1px #d5d5d5"
+//   },
+//   platFormStyle:{
+//     height:"400px",
+//     marginLeft:"20px",
+//     marginTop:"50px",
+//     width:"auto",
+//     borderRadius: "6px",
+//     border: "solid 1px #d5d5d5",
+//     marginLeft:"95px"
+//   },
+//   scrumReport:{
+//     height:"400px",
+//     marginTop:"50px",
+//     width:"1100px",
+//     borderRadius: "6px",
+//     border: "solid 1px #d5d5d5"
+//   }
+// }
+
+
+
+export default class Home extends React.Component {
+  
+  render() {
+  
+      return (
+        <div style={{marginTop:"90px"}}>
+        Millu 
+       
+          </div>
+      )
+    }
+  }
+
+
+
